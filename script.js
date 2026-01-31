905b8d3977a5ee8733dc2a9098e4af8d099ef7ac
Tfleetood <146600187+Tfleetwood@users.noreply.github.com>
2026-01-30 23:22:33 -0800

    Add Computer and Human Functions
    Created two functions: one to generate the computer's choice randomly, and another to get the human player's choice via a prompt.

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-// Get computer choice
-function getComputerChoice()
+// Write logic to get computer choice
+function getComputerChoice() {
+    const computer = Math.floor(Math.random() * 3);
+    if (computer === 0) {
+        return "rock";
+    } else if (computer === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
+
+// Write logic to get human choice
+function getHumanChoice() {
+    prompt("Enter rock, paper, or scissors:");
+}
+console.log(getHumanChoice());
+
+// Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Write logic to play a single round
+function playRound(humanChoice, computerChoice) {
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
